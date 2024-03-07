cfdaf29a92be33509c61bffceeb0ec8dcd4fc63d
Daniela Lara <laradni@icloud.com>
2024-03-07 14:29:55 -0600

    (Add) Operators

---
+++ js/ejemplos/05-operadores.js
@@ -0,0 +1,35 @@
+/* --- Operadores ---
+ * De asiganción
+ * Aritméticos
+ * Lógicos
+ * De comparación
+ */
+
+// Operador de asiganción (=)
+const hola = "Hola";
+
+// Operadores aritméticos
+const suma = 1 + 2;
+const resta = 8 - 3;
+const multiplicacion = 3 * 5;
+const division = 10 / 2;
+const modulo = 4 % 2;
+
+// Operadores lógicos
+const and = true && true; // true
+const or = true || false; // true
+const not = !false; // true
+
+// Operadores de comparación
+const mayor = 5 > 8; // false
+const mayorIgual = 18 >= 10; // true
+const menor = 5 < 8; // true
+const menorIgual = 18 <= 10; // false
+const igualdad = 3 == "3"; // true
+const igualdadEstricta = 3 === "3"; // false
+const diferencia = 5 != "5"; // false
+const diferenciaEstricta = 5 !== "5"; // true
+
+// Null y Undefined
+let numero; // undefinded (esta declarada pero no tiene valor)
+let numeroNull = null; // tipo objeto valor nulo

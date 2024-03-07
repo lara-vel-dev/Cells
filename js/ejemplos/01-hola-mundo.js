1108d1c68fd54af1408d724f56b7900b6be6431a
Daniela Lara <laradni@icloud.com>
2024-03-06 22:44:47 -0600

    (Add) Hello world

---
+++ js/ejemplos/01-hola-mundo.js
@@ -0,0 +1,18 @@
+/* 
+ - Se recomienda colocar el script antes del cierre del body (html) por 
+ temas de performance. No es necesario que las funcionalidades cargen 
+ antes de que cargue la página.
+- Para enlaza un script en html con <script src="ruta"></script>
+- JavaScript te permite manipular el DOM (Document Object Model)
+*/
+
+// Hola mundo con JS
+console.log("Hola mundo!!");
+
+// Esto es un comentario de una sola línea
+
+/*
+ * Esto es un
+ * comentario
+ * multilínea
+ */

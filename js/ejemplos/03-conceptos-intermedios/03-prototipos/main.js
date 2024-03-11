e9cee2e61de96994e54346178daa5168125a7b0a
Daniela Lara <laradni@icloud.com>
2024-03-11 15:09:05 -0600

    (Add) Prototypes

---
+++ js/ejemplos/03-conceptos-intermedios/03-prototipos/main.js
@@ -0,0 +1,54 @@
+// Prototipos
+function Cliente(nombre, saldo) {
+  this.nombre = nombre;
+  this.saldo = saldo;
+}
+
+// Añadir un método Clase.prototype.nombreMetodo = function(){}
+Cliente.prototype.saludo = function () {
+  console.log("Hola desde un prototipo");
+};
+
+Cliente.prototype.tipoCliente = function () {
+  let tipo;
+
+  if (this.saldo >= 10000) {
+    tipo = "Gold";
+  } else if (this.saldo >= 5000) {
+    tipo = "Platinum";
+  } else {
+    tipo = "Normal";
+  }
+
+  console.log(tipo);
+};
+
+Cliente.prototype.infoCliente = function () {
+  console.log(`Nombre: ${this.nombre} Saldo: $${this.saldo.toFixed(2)}`);
+};
+
+Cliente.prototype.retiraSaldo = function (retira) {
+  this.saldo -= retira;
+  console.log(
+    `Haz retirado $${retira.toFixed(
+      2
+    )}\nAhora cuentas con un saldo de $${this.saldo.toFixed(2)}`
+  );
+};
+
+// Instancia
+const dani = new Cliente("Dani", 10000);
+dani.saludo();
+dani.tipoCliente();
+dani.infoCliente();
+dani.retiraSaldo(2500.25);
+
+// Herencia de prototipos
+function Persona(nombre, saldo, telefono) {
+  // Función que trae las propiedades del prototipo que necesitamos
+  Cliente.call(this, nombre, saldo);
+  this.telefono = telefono;
+}
+const persona1 = new Persona("Persona 1", 8500, 1234567890);
+persona1.infoCliente();
+persona1.saludo();
